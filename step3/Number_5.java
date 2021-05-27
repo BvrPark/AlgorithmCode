7ea0db4ae958e007282bd171beb8b1a7ae068e47
crupy <wlxh95@naver.com>
2021-05-28 00:27:40 +0900

    print 1~n

---
+++ step3/Number_5.java
@@ -0,0 +1,17 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Number_5 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+
+		for (int i = 0; i < arr.length; i++) {
+			System.out.println(i + 1);
+		}
+	}
+
+}
