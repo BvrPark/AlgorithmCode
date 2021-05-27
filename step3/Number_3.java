17e60dcd6c82e4e444bc392eeeef815615479f95
crupy <wlxh95@naver.com>
2021-05-28 00:27:08 +0900

    sum 1~n

---
+++ step3/Number_3.java
@@ -0,0 +1,20 @@
+package step3;
+
+import java.util.Scanner;
+
+public class Number_3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int sum = 0;
+
+		for (int i = 1; i <= n; i++) {
+			sum = sum + i;
+
+		}
+		System.out.println(sum);
+
+	}
+
+}
