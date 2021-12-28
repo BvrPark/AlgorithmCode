8855fc0af99a6d15d137135aaa1d9566bff85688
crupy <wlxh95@naver.com>
2021-12-28 23:17:26 +0900

    다시보니 코드가 돌아가는 순서가 잘못된 것 같아서 if문 안의 순서를 조금 바꾸었다.

--- src/step8/Num1_1712.java
+++ src/step8/Num1_1712.java
@@ -11,12 +11,12 @@ public class Num1_1712 {
         int count = 0;                          //판매갯수
 
         while(true){
-            if(A>=(C-B)*count){                 //순이익(C-B)*판매갯수가 고정비용보다 작거나 같을때
+
+            if((C-B)<=0) {                   //그 중에서도 순이익(C-B)가 적자일 경우
+                System.out.println(-1);     //총수입이 흑자가 안나므로 -1출력
+                break;
+            }else if(A>=(C-B)*count){           //순이익(C-B)*판매갯수가 고정비용보다 작거나 같을때
                 count++;                        //물건을 1개 더 판다.
-                if((C-B)<=0){                   //그 중에서도 순이익(C-B)가 적자일 경우
-                    System.out.println(-1);     //총수입이 흑자가 안나므로 -1출력
-                    break;
-                }
             }else if(A<(C-B)*count){            //순이익(C-B)*판매갯수가 고정비용보다 많을때 = 총수입이 흑자일때
                 System.out.println(count);      //판매갯수를 출력
                 break;
