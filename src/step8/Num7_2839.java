8ccd8914d98355c091ead3458dd69755a9adb020
crupy <wlxh95@naver.com>
2022-01-18 13:38:21 +0900

    백준알고리즘 8단계의 7번문제인 설탕배달 문제
    자세한 풀이는 https://crupy.tistory.com/41에 적어놨다.

---
+++ src/step8/Num7_2839.java
@@ -0,0 +1,26 @@
+package step8;
+
+import java.util.Scanner;
+
+public class Num7_2839 {
+    public int solution(int N) {                //자세한 풀이는 블로그에
+        if (N % 5 == 0) {
+            return N / 5;
+        }else if (N == 4 || N == 7) {
+            return -1;
+        }else if (N % 5 == 1 || N % 5 == 3) {
+            return (N / 5) + 1;
+        }else if (N % 5 == 2 || N % 5 == 4) {
+            return (N / 5) + 2;
+        }else{
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Num7_2839 test = new Num7_2839();
+        int N = sc.nextInt();
+        System.out.println(test.solution(N));
+    }
+}
