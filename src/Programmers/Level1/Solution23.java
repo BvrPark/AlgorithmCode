b6ab7db3a189c520c93489daf396564a0f2e32d0
crupy <wlxh95@naver.com>
2022-02-19 19:58:55 +0900

    프로그래머스 Level1 짝수와 홀수 문제
    자세한 풀이는 주석에

---
+++ src/Programmers/Level1/Solution23.java
@@ -0,0 +1,7 @@
+package Programmers.Level1;
+
+public class Solution23 {               //짝수와 홀수
+    public String solution(int num) {
+        return (num%2==0?"Even":"Odd"); //짝수면 Even, 홀수면 Odd 출력
+    }
+}
