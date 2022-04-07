f4810e5c03462c7d200f25c82f25eb40f177018c
crupy <wlxh95@naver.com>
2022-04-07 23:40:15 +0900

    프로그래머스 Level2 튜플 문제

---
+++ src/Programmers/Level2/Solution26.java
@@ -0,0 +1,40 @@
+package Programmers.Level2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Solution26 {
+    public int[] solution(String s) {       //튜플
+       s = s.substring(2, s.length() - 2).replace("},{", "-");
+
+        String[] arr = s.split("-");
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.length() - o2.length();
+            }
+        });
+
+        ArrayList<Integer> list = new ArrayList<>();
+        for (String temp : arr) {
+            String[] val = temp.split(",");
+
+            for (int i = 0; i < val.length; i++) {
+                int num = Integer.valueOf(val[i]);
+
+                if (!list.contains(num)) {
+                    list.add(num);
+                }
+            }
+        }
+
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+
+        return answer;
+    }
+}
