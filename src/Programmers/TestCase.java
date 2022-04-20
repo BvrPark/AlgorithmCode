cfd20b4ce845f6e050b9f24d21c99bb4daed2be9
crupy <wlxh95@naver.com>
2022-04-20 18:58:46 +0900

    프로그래머스 Level2 푸는 중

--- src/Programmers/TestCase.java
+++ src/Programmers/TestCase.java
@@ -1,4 +1,11 @@
 package Programmers;
 
+import java.util.*;
+
 public class TestCase {
+    public long[] solution(long[] numbers) {    //2개 이하로 다른 비트
+        long[] answer = {};
+        return answer;
+    }
+
 }
