84a4541896de7683eaf30475cfb4e69538ac6836
crupy <wlxh95@naver.com>
2022-06-05 23:45:11 +0900

    프로그래머스 Level2 2xn타일링 문제
    간단한 풀이는 주석에 자세한 풀이는 블로그에 기재

--- src/Programmers/Level2/Solution33.java
+++ src/Programmers/Level2/Solution33.java
@@ -2,10 +2,10 @@ package Programmers.Level2;
 
 public class Solution33 {           // 2xn타일링 문제
     public int solution(int n) {
-        int[] com = new int[n+1];
-        com[1] = 1;
-        com[2] = 2;
-        for(int i = 3; i <=n ;i++){
+        int[] com = new int[n+1];   // 입력한 길이보다 1많게 배열 생성
+        com[1] = 1;                 // 길이가 1일때, 경우의 수
+        com[2] = 2;                 // 길이가 2일때, 경우의 수
+        for(int i = 3; i <=n ;i++){ // 길이가 3이상일 때, 경우의 수
             com[i] = (com[i-1]+com[i-2])%1000000007;
         }
         return com[n];
