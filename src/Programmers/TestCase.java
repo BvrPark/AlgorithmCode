3d49559fd34af2a01b4e6a6ea10bf9387573ab10
crupy <wlxh95@naver.com>
2022-07-19 23:36:59 +0900

    문자열 압축에 대한 간단한 테스트 케이스

--- src/Programmers/TestCase.java
+++ src/Programmers/TestCase.java
@@ -3,13 +3,18 @@ package Programmers;
 import java.util.*;
 
 public class TestCase {
-    public ArrayList<String> solution(String[] orders, int[] course) {
-        ArrayList<String> answer = new ArrayList<>();   //정답을 담을 배열 생성
-        Map<String,Integer> check = new HashMap<>();    //빈도수 체크할 Map생성
-        for(String s : orders){
-            char[] ch_arr = s.toCharArray();            //문자열을 문자배열로 바꿈
-            Arrays.sort(ch_arr);                        //문자배열들을 오름차순으로 정렬
-            s = String.valueOf(ch_arr);                 //다시 문자열로 만들어줌
+    public int solution(String s) {
+        int answer = 0;
+        int count = 0;
+        String f = "";
+        String e = "";
+        String emp = "";
+        for(int i = 0 ; i < s.length();i++){
+            count++;
+            
+            for(int j = 1; j <= s.length();j++){
+
+            }
         }
         return answer;
     }
@@ -18,9 +23,7 @@ public class TestCase {
 
     public static void main(String[] args) {
         TestCase test = new TestCase();
-        String[] arr = {"ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"};
-        Arrays.sort(arr);
-        System.out.println(Arrays.toString(arr));
+
     }
 
 
