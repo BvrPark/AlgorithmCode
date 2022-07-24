aa624a5e1d775f9a920c01eab986bf8c4bdae716
crupy <wlxh95@naver.com>
2022-07-24 22:27:17 +0900

    프로그래머스 Level2 큰 수 만들기 문제
    1. 주석 추가
    2. 5번째 줄에 StringBuilder()에서 괄호 안에 ""은 필요가 없어서 빼주었다.

--- src/Programmers/Level2/Solution41.java
+++ src/Programmers/Level2/Solution41.java
@@ -2,11 +2,11 @@ package Programmers.Level2;
 
 public class Solution41 {
     public String solution(String number, int k) {                  //큰 수 만들기
-        StringBuilder answer = new StringBuilder("");
-        int len = number.length() - k;
-        int start = 0;
+        StringBuilder answer = new StringBuilder();                 // 실행속도 때문에 StringBuilder를 따로 만듬
+        int len = number.length() - k;                              // 만들 수 있는 숫자의 길이
+        int start = 0;                                              // 시작지점
 
-        while(start < number.length() && answer.length() != len) {
+        while(start < number.length() && answer.length() != len) {  // 자세한 풀이는 블로그에 기재
             int leftNum = k + answer.length() + 1;
             int max = 0;
             for (int j = start; j < leftNum; j++) {
