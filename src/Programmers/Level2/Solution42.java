807faae22211e0b5cabb68be05f55a8697ac1737
crupy <wlxh95@naver.com>
2022-07-25 23:45:27 +0900

    프로그래머스 Level2 3 x n 타일링 문제
    주석에 설명을 조금 추가하였다.

--- src/Programmers/Level2/Solution42.java
+++ src/Programmers/Level2/Solution42.java
@@ -7,8 +7,8 @@ public class Solution42 {
             return 0;
         }
         long plus = 0;
-        long[] dp = new long[n+1];
-        dp[0] = 1;
+        long[] dp = new long[n+1];              
+        dp[0] = 1;                              //첫번째 케이스와 두번째 케이스의 수를 미리 설정
         dp[2] = 3;
         for(int i=4; i<=n; i+=2) {              //규칙에 따라서 dp[i]에 들어갈 수 있는 경우의 수를 대입
             plus += (2 * dp[i-4]) % num;
