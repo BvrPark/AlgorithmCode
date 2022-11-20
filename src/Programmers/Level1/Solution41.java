29f4295195e163235053c11327542a29f3e53a87
-crupy- <wlghsms95@gmail.com>
2022-11-20 23:44:12 +0900

    프로그래머스 Level1 푸드파이트 대회
    자세한 풀이는 블로그에 기재

--- src/Programmers/Level1/Solution41.java
+++ src/Programmers/Level1/Solution41.java
@@ -6,7 +6,7 @@ public class Solution41 {
         String answer = "0";                    //answer을 xxx0xxx형태로 만들어주기 위해 미리 0을 answer에 대입
         int food_n;                             //food의 갯수를 임시로 저장할 변수 생성
         for(int i = food.length-1; i >= 1;i--){ //for문 안의 이유는 블로그에 기재
-            food_n = food[i]/2;                 
+            food_n = food[i]/2;
             for(int j = 0; j<food_n;j++){
                 answer = i + answer + i;
             }
