d85b0170685935c408597ae614c5ceeff2f54983
-crupy- <wlghsms95@gmail.com>
2022-11-20 23:43:09 +0900

    프로그래머스 Level2 우박수열 정적분 문제
    자세한 풀이는 블로그에 기재

--- src/Programmers/Level2/S47.java
+++ src/Programmers/Level2/S47.java
@@ -1,16 +1,20 @@
 package Programmers.Level2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // 우박수열 정적분
 public class S47 {
     public double[] solution(int k, int[][] ranges) {
         double[] answer = new double[ranges.length];    //답 저장할 배열
-        long[][] matrix = new long[ranges.length][2];   //좌표를 저장할 배열
-        long[] area = new long[ranges.length];          //구간 별 누적 넓이를 저장할 배열
-        int num = 0;
-        while(matrix[num][2] != 1){
-            matrix[num][1] = k;
-            if(k){
-
+        List<Double> area = new ArrayList<>();          //구간 별 누적 넓이를 저장할 List
+        int prev = k;
+        area.add(0.0);
+        while(k != 1){
+            if(k%2 == 0){
+                k /= 2;
+            }else{
+                k = k*3 + 1;
             }
         }
         return answer;
