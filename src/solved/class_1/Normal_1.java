00708957bc4f3020cd4930dc139750438136d2d7
-crupy- <wlghsms95@gmail.com>
2022-11-21 19:28:46 +0900

    solved.ac의 class1에 있는 검증 수 문제
    백준 알고리즘의 solved.ac의 class1에 있는 기본 문제이다.

--- src/solved/class_1/Normal_1.java
+++ src/solved/class_1/Normal_1.java
@@ -1,2 +1,16 @@
-package solved.class_1;public class Normal_1 {
+package solved.class_1;
+
+import java.util.Scanner;
+
+//검증 수
+public class Normal_1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for(int i = 0; i<5; i++){
+            sum += Math.pow(sc.nextInt(),2);    //값을 입력 받을 때마다 제곱을 해서 sum에 더한다.
+        }
+        System.out.println(sum%10);             //sum을 10으로 나누고 나머지 값을 출력
+    }
 }
