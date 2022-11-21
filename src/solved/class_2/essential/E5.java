d42be69f50d53b4d6eedd66c09b17c328f0de129
-crupy- <wlghsms95@gmail.com>
2022-11-21 19:34:00 +0900

    solved.ac의 class2 문제 중 직각 삼각형 문제
    주석으로 간단히 설명해 놓았다.

--- src/solved/class_2/essential/E5.java
+++ src/solved/class_2/essential/E5.java
@@ -1,2 +1,27 @@
-package solved.class_2.essential;public class E5 {
+package solved.class_2.essential;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+//직각삼각형
+public class E5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[3];
+        while(true){
+            arr[0] = sc.nextInt();
+            arr[1] = sc.nextInt();
+            arr[2] = sc.nextInt();
+            if(arr[0] == 0){
+                break;
+            }
+            Arrays.sort(arr);           //어떤 배열에 넣지 않아도 arr자체가 정렬이 된다.
+            if(Math.pow(arr[0],2)+Math.pow(arr[1],2) == Math.pow(arr[2],2)){    //피타고라스 법칙
+                System.out.println("right");
+            }else{
+                System.out.println("wrong");
+            }
+
+        }
+    }
 }
