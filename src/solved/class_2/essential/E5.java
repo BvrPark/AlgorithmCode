8eae80bf87603cf48be52c2eb9ee0986f3a0ff9b
-crupy- <wlghsms95@gmail.com>
2022-11-25 19:59:10 +0900

    백준 알고리즘 4153번 직각삼각형 문제
    코드에 대한 설명을 주석으로 적어놓음

--- src/solved/class_2/essential/E5.java
+++ src/solved/class_2/essential/E5.java
@@ -3,19 +3,25 @@ package solved.class_2.essential;
 import java.util.Arrays;
 import java.util.Scanner;
 
-//직각삼각형
+//직각삼각형(4153번)
 public class E5 {
     public static void main(String[] args) {
+        //입력 세팅
         Scanner sc = new Scanner(System.in);
+        //세 변의 길이를 저장할 배열
         int[] arr = new int[3];
+
         while(true){
+            //세변의 길이를 입력 받음
             arr[0] = sc.nextInt();
             arr[1] = sc.nextInt();
             arr[2] = sc.nextInt();
+            //처음 입력 받는 변의 길이가 0이면 반복문 탈출
             if(arr[0] == 0){
                 break;
             }
-            Arrays.sort(arr);           //어떤 배열에 넣지 않아도 arr자체가 정렬이 된다.
+            //길이를 오름차순으로 정렬
+            Arrays.sort(arr);
             if(Math.pow(arr[0],2)+Math.pow(arr[1],2) == Math.pow(arr[2],2)){    //피타고라스 법칙
                 System.out.println("right");
             }else{
