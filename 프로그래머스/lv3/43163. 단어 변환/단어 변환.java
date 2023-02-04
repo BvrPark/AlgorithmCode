c214c4b87f1c8aa2e6fb403704743eac04390b9e
ParkJiHo <devcrupy@gmail.com>
2023-02-04 19:25:09 +0900

    [level 3] Title: 단어 변환, Time: 0.03 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/lv3/43163. 단어 변환/단어 변환.java
@@ -1,59 +1,41 @@
-import java.util.*;
-
-class Node{
-    private String str;
-    private int count;
-    
-    public Node(String str, int count){
-        this.str = str;
-        this.count = count;
-    }
-    
-    public String getS(){
-        return this.str;
-    }
-    public int getC(){
-        return this.count;
-    }
-}
-
 class Solution {
     static boolean[] check;
-    
+    static int answer = 0;
+
     public int solution(String begin, String target, String[] words) {
         check = new boolean[words.length];
-        return bfs(begin, target, words);
+        bfs(begin, target, words, 0);
+        return answer;
     }
-    
-    public int bfs(String begin, String target, String[] words){
-        Queue<Node> q = new LinkedList<>();
-        int differ = 0;
-        q.offer(new Node(begin,0));
-        
-        while(!q.isEmpty()){
-            Node node = q.poll();
-            if(node.getS().equals(target)){
-                return node.getC();
-            }
-            
-            for(int i = 0; i < words.length; i++){
-                differ = 0;
-                for(int j = 0; j < words[i].length(); j++){
-                    if(node.getS().charAt(j) != words[i].charAt(j)){
-                        differ += 1;
-                    }
-                }
-                
-                if(differ == 1 && check[i] == false){
-                    check[i] = true;
-                    q.offer(new Node(words[i],node.getC()+1));
-                }
-            }
-            
+
+    public void bfs(String begin, String target, String[] words, int count){
+        if(begin.equals(target)){
+            answer = count;
+            return;
         }
-        
-        return 0;
-        
-        
+
+        int differ = 0;
+
+        for(int i = 0; i < words.length; i++){
+            if(check[i]){
+                continue;
+            }
+
+            differ = 0;
+
+            for(int j = 0; j < begin.length(); j++){
+                if(begin.charAt(j) != words[i].charAt(j)){
+                    differ++;
+                }
+            }
+
+            if(differ == 1){
+                check[i] = true;
+                bfs(words[i],target,words,count+1);
+                check[i] = false;
+            }
+        }
+
+
     }
 }
